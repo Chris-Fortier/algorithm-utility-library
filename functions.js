bb13d3bd295e6f4a5724601a240777f2f1162cf9
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-19 11:05:13 -0700

    add convertToF function

--- functions.js
+++ functions.js
@@ -1,1 +1,35 @@
-// Your functions go here!
+// Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+   let fahrenheit = celsius * (9 / 5) + 32;
+   return fahrenheit;
+}
+
+// Reverse a String
+
+// Factorialize a Number
+
+// Find the Longest Word in a String
+
+// Return Largest Numbers in Arrays
+
+// Confirm the Ending
+
+// Repeat a String Repeat a String
+
+// Truncate a String
+
+// Finders Keepers
+
+// Boo who
+
+// Title Case a Sentence
+
+// Slice and Splice
+
+// Falsy Bouncer
+
+// Where do I Belong
+
+// Mutations
+
+// Chunky Monkey
